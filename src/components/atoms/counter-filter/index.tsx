bbf3fc9537e3ceefa925dff36ddee6dbb109d1bb
FedericoBen <federicobg95@gmail.com>
2024-12-13 17:38:38 -0500

    [BUGFIX] Center info counter filter

--- src/components/atoms/counter-filter/index.tsx
+++ src/components/atoms/counter-filter/index.tsx
@@ -4,7 +4,7 @@ interface CounterFiltersProps {
 
 const CounterFilters = ({ count }: CounterFiltersProps) => {
   return (
-    <section className="bg-secondary600 bg-opacity-20 min-w-min-[64px] min-h-[24px] rounded-[12px] pr-[12px] pl-[12px]">
+    <section className="bg-secondary600 bg-opacity-20 min-w-min-[64px] min-h-[24px] rounded-[12px] pr-[12px] pl-[12px] flex items-center justify-center">
       <p className="text-custom_green text-[14px] font-greycliff font-medium">{`${count} filter${
         count > 1 ? "s" : ""
       }`}</p>
