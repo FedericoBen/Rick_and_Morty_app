944d9087002d0f6b8abc00becb34f82606c8bb50
FedericoBen <federicobg95@gmail.com>
2024-12-13 21:51:37 -0500

    [FIX] Bug props

--- src/components/molecules/filter-menu/index.tsx
+++ src/components/molecules/filter-menu/index.tsx
@@ -1,23 +1,14 @@
-import { useState } from "react";
 import FilterMenuDesktop from "../filter-menu-desktop";
 import ButtonSearch from "../../atoms/button-search";
 import { useNavigate } from "react-router-dom";
 import ROUTES from "../../../constants/routes";
 
-
 const FilterMenu = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
   const navigate = useNavigate();
-
-
 
   return (
     <div>
-      <FilterMenuDesktop
-        open={menuOpen}
-        onOpen={() => setMenuOpen(true)}
-        onClose={() => setMenuOpen(false)}
-      />
+      <FilterMenuDesktop />
       <ButtonSearch
         className="md:hidden"
         onClick={() => {
