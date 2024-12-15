ca4e6cfacb95d69f80ffb87f22896e737aae3f20
FedericoBen <federicobg95@gmail.com>
2024-12-15 15:48:29 -0500

    [FEATURE] Modify vite config to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
   base: "https://FedericoBen.github.io/Rick_and_Morty_app",
 });
